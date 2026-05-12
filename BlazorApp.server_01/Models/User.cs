5fe88dd696afb7d1952bf9b5dd985f04676fe8af
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-05-12 17:29:30 +0630

    Add project files.

---
+++ BlazorApp.server_01/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp.server_01.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
